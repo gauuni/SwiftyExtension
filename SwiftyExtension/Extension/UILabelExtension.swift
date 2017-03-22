fdff91a62dd4a2b5ab62f15f24445f89439459bc
KhoiNguyen <nguyenkhoinguyen082444@gmail.com>
2017-03-22 11:26:03 +0700

    Add UILabel and UITextfield extension

---
+++ SwiftyExtension/Extension/UILabelExtension.swift
@@ -0,0 +1,45 @@
+//
+//  UILabelExtension.swift
+//  GoldenTime
+//
+//  Created by Liem Ly Quan on 7/15/16.
+//  Copyright © 2016 CAN. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public extension UILabel {
+
+    /*
+     set label text with custom spacing
+     **/
+    func setTextWithCustomCharacterSpacing(text: String, spacing: CGFloat){
+        let attributedString = NSMutableAttributedString(string: text)
+        attributedString.addAttribute(
+            NSKernAttributeName,
+            value: CGFloat(spacing),
+            range: NSRange(location: 0, length: text.characters.count))
+        self.attributedText = attributedString
+    }
+    
+    
+    func setText(text: String="",
+                 withColor color: UIColor = UIColor.black,
+                 textAlignment: NSTextAlignment = .left,
+                 fontName: String="Lato-Regular",
+                 fontSize: CGFloat=UIFont.labelFontSize){
+        self.text = text
+        self.textColor = color
+        self.textAlignment = textAlignment
+        self.font = UIFont(name: fontName, size: fontSize)
+    }
+    
+    func add(target: AnyObject, action: Selector){
+        let tapGesture = UITapGestureRecognizer(target: target, action: action)
+        tapGesture.numberOfTapsRequired = 1
+        tapGesture.numberOfTouchesRequired = 1
+        self.addGestureRecognizer(tapGesture)
+        self.isUserInteractionEnabled = true
+    }
+}
