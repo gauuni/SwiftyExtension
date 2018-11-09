02e99502e22e803ca756a70a33ffa66786790e73
Khoi Nguyen <nguyenkhoinguyen082444@gmail.com>
2018-11-09 15:22:20 +0700

    update for <Swift 4.1

--- SwiftyExtension/Extension/ArrayExtension.swift
+++ SwiftyExtension/Extension/ArrayExtension.swift
@@ -41,7 +41,7 @@ public extension MutableCollection {
         
         for (firstUnshuffled, unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
             // Change `Int` in the next line to `IndexDistance` in < Swift 4.1
-            let d: Int = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
+            let d: IndexDistance = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
             let i = index(firstUnshuffled, offsetBy: d)
             swapAt(firstUnshuffled, i)
         }
